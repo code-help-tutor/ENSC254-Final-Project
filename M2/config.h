18c5d20829829e3c8cc1cbf58a56dfacb6c726b6
Code Help Tutor <1823890830@qq.com>
2024-07-20 07:54:32 +0000

    WeChat cstutorcs ENSC254 Final Project

---
+++ M2/config.h
@@ -0,0 +1,33 @@
+WeChat: cstutorcs
+QQ: 749389476
+Email: tutorcs@163.com
+#ifndef __CONFIG_H__
+#define __CONFIG_H__
+
+// For each test, uncomment all its macros, and disable all other macros.
+// macros for MS3 will be added later
+
+// required for MS1 (test_simulator_ms1.sh)
+//#define DEBUG_CACHE_TRACE	// prints the cache trace at the end of program
+// enable `DEBUG_CYCLE` this after completing the code in each stage
+//#define DEBUG_CYCLE
+//#define MEM_LATENCY 0		// before ms3, we ignore memory access latency
+
+// required for MS2 (test_simulator_ms2.sh)
+#define DEBUG_REG_TRACE
+#define DEBUG_CYCLE
+#define PRINT_STATS		// prints overall stats
+#define MEM_LATENCY 0
+
+// required for MS2: vec_xprod.input (test_simulator_ms2_extended.sh)
+//#define PRINT_STATS
+//#define MEM_LATENCY 0
+
+// required for MS3: (test_simulator_ms3.sh)
+//#define DEBUG_REG_TRACE
+//#define DEBUG_CYCLE
+//#define PRINT_STATS
+//#define DEBUG_CACHE_TRACE	// prints the cache trace at the end of program
+//#define MEM_LATENCY 10
+
+#endif // __CONFIG_H__
---
+++ M2/test_simulator_ms2_extended.sh
@@ -0,0 +1,10 @@
+WeChat: cstutorcs
+QQ: 749389476
+Email: tutorcs@163.com
+# tests below are scored
+# remember to update config.h accordingly before running this!
+
+# full version of vec_xprod only contains the stats of the program, not the entire reg-trace
+./riscv -s -e -f ./code/ms2/input/vec_xprod.input > ./code/ms2/out/vec_xprod.trace
+echo "diff ./code/ms2/ref/vec_xprod.trace ./code/ms2/out/vec_xprod.trace"
+diff ./code/ms2/ref/vec_xprod.trace ./code/ms2/out/vec_xprod.trace
